a41b16cc717a5cf21d20e97d7cc34fa99e286934
cleversamerr <thedev.samer@gmail.com>
2023-05-25 09:31:37 +0300

    made about screen responsive

--- src/screens/common/AboutScreen.jsx
+++ src/screens/common/AboutScreen.jsx
@@ -8,9 +8,32 @@ import {
 import useLocale from "../../hooks/useLocale";
 import DefaultScreenTitle from "../../components/screenTitles/DefaultScreenTitle";
 import NetworkStatusLine from "../../components/common/NetworkStatusLine";
+import useScreen from "../../hooks/useScreen";
 
 export default function AboutScreen({ navigation }) {
+  const screen = useScreen();
   const { i18n } = useLocale();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: screen.getHorizontalPixelSize(15),
+      paddingVertical: screen.getVerticalPixelSize(15),
+      paddingTop: screen.getVerticalPixelSize(50),
+      gap: screen.getVerticalPixelSize(40),
+    },
+    image: {
+      alignSelf: "center",
+      width: screen.getHorizontalPixelSize(250),
+      height: screen.getVerticalPixelSize(250),
+      backgroundColor: "red",
+      marginBottom: screen.getVerticalPixelSize(20),
+    },
+    text: {
+      fontFamily: "cairo-500",
+      fontSize: 14,
+    },
+  });
 
   const handleGoBack = () => {
     try {
@@ -37,22 +60,3 @@ export default function AboutScreen({ navigation }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 15,
-    paddingTop: 50,
-    gap: 30,
-  },
-  image: {
-    width: 250,
-    height: 250,
-    alignSelf: "center",
-    marginBottom: 20,
-  },
-  text: {
-    fontFamily: "cairo-500",
-    fontSize: 14,
-  },
-});
