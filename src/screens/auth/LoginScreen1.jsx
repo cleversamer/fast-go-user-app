e86f2ae045c5fff232b9d7344048c6a7818f5efd
cleversamerr <thedev.samer@gmail.com>
2023-05-25 09:01:19 +0300

    made login screen 1 responsive

--- src/screens/auth/LoginScreen1.jsx
+++ src/screens/auth/LoginScreen1.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { SafeAreaView, ScrollView, View, Text, StyleSheet } from "react-native";
-import * as theme from "../../constants/theme";
 import PhoneInput from "../../components/inputs/PhoneInput";
 import CustomButton from "../../components/buttons/CustomButton";
 import HorizontalLines from "../../components/common/HorizontalLines";
@@ -9,11 +8,66 @@ import useLocale from "../../hooks/useLocale";
 import NetworkStatusLine from "../../components/common/NetworkStatusLine";
 import screens from "../../static/screens.json";
 import checkPhoneNSN from "../../utils/checkPhoneNSN";
+import useScreen from "../../hooks/useScreen";
 
 export default function LoginScreen1({ navigation, route }) {
+  const screen = useScreen();
   const { role } = route.params;
   const { i18n } = useLocale();
   const [phone, setPhone] = useState({ icc: "+218", nsn: "" });
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: screen.getHorizontalPixelSize(15),
+      paddingVertical: screen.getVerticalPixelSize(15),
+      paddingTop: screen.getVerticalPixelSize(50),
+    },
+    title: {
+      fontFamily: "cairo-800",
+      fontSize: 28,
+      marginBottom: screen.getVerticalPixelSize(5),
+    },
+    subtitle: {
+      fontFamily: "cairo-600",
+      fontSize: 16,
+      color: "#747474",
+    },
+    phoneContainer: {
+      marginTop: screen.getVerticalPixelSize(60),
+      marginBottom: screen.getVerticalPixelSize(20),
+    },
+    buttonContainer: {
+      paddingVertical: screen.getVerticalPixelSize(12),
+    },
+    buttonText: {
+      fontFamily: "cairo-800",
+      fontSize: 16,
+    },
+    horizontalLinesContainer: {
+      marginVertical: screen.getVerticalPixelSize(25),
+    },
+    horizontalLinesText: {
+      fontFamily: "cairo-700",
+      marginHorizontal: screen.getHorizontalPixelSize(5),
+    },
+    loginButtonsContainer: {
+      gap: screen.getVerticalPixelSize(15),
+    },
+    conditionsText: {
+      fontFamily: "cairo-400",
+      marginTop: screen.getVerticalPixelSize(30),
+      color: "#747474",
+    },
+    privacyPolicyText: {
+      fontFamily: "cairo-400",
+      marginTop: screen.getVerticalPixelSize(30),
+      color: "#747474",
+    },
+    blueText: {
+      color: "#0038FF",
+    },
+  });
 
   const handlePhoneNSNChange = (phoneNSN) => {
     try {
@@ -111,56 +165,3 @@ export default function LoginScreen1({ navigation, route }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 15,
-    paddingTop: 50,
-  },
-  title: {
-    fontSize: 22,
-    fontFamily: "cairo-800",
-    fontSize: 28,
-    marginBottom: 5,
-  },
-  subtitle: {
-    fontFamily: "cairo-600",
-    fontSize: 16,
-    color: "#747474",
-  },
-  phoneContainer: {
-    marginTop: 60,
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    paddingVertical: 12,
-  },
-  buttonText: {
-    fontFamily: "cairo-800",
-    fontSize: 16,
-  },
-  horizontalLinesContainer: {
-    marginVertical: 25,
-  },
-  horizontalLinesText: {
-    fontFamily: "cairo-700",
-    marginHorizontal: 5,
-  },
-  loginButtonsContainer: {
-    gap: 15,
-  },
-  conditionsText: {
-    fontFamily: "cairo-400",
-    marginTop: 30,
-    color: "#747474",
-  },
-  privacyPolicyText: {
-    fontFamily: "cairo-400",
-    marginTop: 30,
-    color: "#747474",
-  },
-  blueText: {
-    color: "#0038FF",
-  },
-});
