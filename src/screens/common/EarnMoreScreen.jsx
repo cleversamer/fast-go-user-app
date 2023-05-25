35d2bd06a1334bc5e34bd30266ab0762ccd07576
cleversamerr <thedev.samer@gmail.com>
2023-05-25 09:37:44 +0300

    made earn more screen responsive

--- src/screens/common/EarnMoreScreen.jsx
+++ src/screens/common/EarnMoreScreen.jsx
@@ -13,11 +13,83 @@ import * as theme from "../../constants/theme";
 import useClipboard from "../../hooks/useClipboard";
 import NetworkStatusLine from "../../components/common/NetworkStatusLine";
 import useAuth from "../../auth/useAuth";
+import useScreen from "../../hooks/useScreen";
 
 export default function EarnMoreScreen({ navigation }) {
+  const screen = useScreen();
   const { user } = useAuth();
   const { i18n } = useLocale();
   const { copyText, showCopiedIcon } = useClipboard();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      paddingHorizontal: screen.getHorizontalPixelSize(15),
+      paddingVertical: screen.getVerticalPixelSize(15),
+      paddingTop: screen.getVerticalPixelSize(50),
+    },
+    inviteImage: {
+      width: screen.getHorizontalPixelSize(200),
+      height: screen.getVerticalPixelSize(200),
+      marginTop: screen.getVerticalPixelSize(35),
+    },
+    title: {
+      fontFamily: "cairo-700",
+      fontSize: 18,
+      marginTop: screen.getVerticalPixelSize(20),
+      textTransform: "capitalize",
+    },
+    subtitle: {
+      fontFamily: "cairo-500",
+      fontSize: 14,
+      marginTop: screen.getVerticalPixelSize(10),
+      textAlign: "center",
+      color: "#747474",
+    },
+    boxContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      backgroundColor: theme.primaryColorLight,
+      width: "100%",
+      paddingHorizontal: screen.getHorizontalPixelSize(25),
+      marginTop: screen.getVerticalPixelSize(30),
+      height: screen.getVerticalPixelSize(150),
+      borderRadius: 6,
+    },
+    giftImage: {
+      width: screen.getHorizontalPixelSize(45),
+      height: screen.getVerticalPixelSize(56),
+    },
+    codeContainer: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      maxWidth: screen.getHorizontalPixelSize(180),
+      gap: screen.getHorizontalPixelSize(7),
+      backgroundColor: theme.primaryColor,
+      borderRadius: 30,
+      paddingVertical: screen.getVerticalPixelSize(3),
+    },
+    codeText: {
+      fontFamily: "cairo-700",
+      fontSize: 14,
+      color: "#fff",
+    },
+    copyIcon: {
+      fontSize: 20,
+      color: "#fff",
+    },
+    giftHintText: {
+      fontFamily: "cairo-500",
+      fontSize: 14,
+      color: "#747474",
+      marginTop: screen.getVerticalPixelSize(35),
+      textAlign: "center",
+    },
+  });
 
   const handleGoBack = () => {
     try {
@@ -70,72 +142,3 @@ export default function EarnMoreScreen({ navigation }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 15,
-    paddingTop: 50,
-    alignItems: "center",
-  },
-  inviteImage: {
-    width: 200,
-    height: 200,
-    marginTop: 35,
-  },
-  title: {
-    fontFamily: "cairo-700",
-    fontSize: 18,
-    marginTop: 20,
-    textTransform: "capitalize",
-  },
-  subtitle: {
-    fontFamily: "cairo-500",
-    fontSize: 14,
-    marginTop: 10,
-    textAlign: "center",
-    color: "#747474",
-  },
-  boxContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: theme.primaryColorLight,
-    width: "100%",
-    paddingHorizontal: 25,
-    marginTop: 30,
-    height: 150,
-    borderRadius: 6,
-  },
-  giftImage: {
-    width: 45,
-    height: 56,
-  },
-  codeContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    maxWidth: 180,
-    gap: 7,
-    backgroundColor: theme.primaryColor,
-    borderRadius: 30,
-    paddingVertical: 3,
-  },
-  codeText: {
-    fontFamily: "cairo-700",
-    fontSize: 14,
-    color: "#fff",
-  },
-  copyIcon: {
-    fontSize: 20,
-    color: "#fff",
-  },
-  giftHintText: {
-    fontFamily: "cairo-500",
-    fontSize: 14,
-    color: "#747474",
-    marginTop: 35,
-    textAlign: "center",
-  },
-});
